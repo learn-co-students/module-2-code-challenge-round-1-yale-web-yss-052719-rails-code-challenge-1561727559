f59bb12ec3df508c170a458e78eba5d0adcecd2c
jqzheng617 <jenna.zheng617@gmail.com>
2019-06-28 10:44:24 -0400

    Code Challenge 2

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,33 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.valid?
+      redirect_to heroine_path(@heroine)
+    else
+      flash.now[:error] = @heroine.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    @powers = Power.all
+
+  end
+
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
--- app/models/power.rb
+++ app/models/power.rb
@@ -1,2 +1,5 @@
 class Power < ApplicationRecord
+  has_many :heroines
+  validates :name, :description, presence: true
+  #need to add only one validates
 end
