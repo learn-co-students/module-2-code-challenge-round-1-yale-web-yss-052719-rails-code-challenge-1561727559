d3f6bff9ebfa362d8dd4b1c1f98c4362b7917fc8
MatthewP01 <matthew.peoples@yale.edu>
2019-06-28 09:55:20 -0400

    Finished the deliverables

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,35 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    render :show
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+    render :new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroines_path
+    else
+      flash[:error] = @heroine.errors.full_messages
+      @powers = Power.all
+      render :new
+    end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
+
 end
