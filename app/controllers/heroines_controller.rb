ec33ec40bce4509cc9245f9a63407f57ad7783d7
Peter <cirediatpl@gmail.com>
2019-06-28 10:31:43 -0400

    Added stretch features: search bar, edit form, delete link, nav bar for indices, link to new heroine form on heroine index"

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,6 +1,10 @@
 class HeroinesController < ApplicationController
   def index
-    @heroines = Heroine.all
+    if params[:power]
+      @heroines = Heroine.where(power_id: Power.find_by(name: params[:power]).id)
+    else
+      @heroines = Heroine.all
+    end
   end
 
   def show
@@ -9,7 +13,7 @@ class HeroinesController < ApplicationController
 
   def new
     @heroine = Heroine.new
-    @powers = Power.all
+    all_powers
   end
 
   def create
@@ -18,15 +22,44 @@ class HeroinesController < ApplicationController
       redirect_to heroine_path(@heroine)
     else
       flash.now[:error] = @heroine.errors.full_messages
-      @powers = Power.all
+      all_powers
       render :new
     end
+  end
+
+## additional RESTful routes -- edit, update, delete -- are added below for the sake of practice
+
+  def edit
+    find_heroine
+    all_powers
+  end
+
+  def update
+    find_heroine
+    @heroine.update(heroine_params)
+    if @heroine.valid?
+      redirect_to heroine_path(@heroine)
+    else
+      flash.now[:error] = @heroine.errors.full_messages
+      all_powers
+      render :edit
+    end
+  end
+
+  def destroy
+    find_heroine
+    @heroine.destroy
+    redirect_to heroines_path
   end
 
   private
 
   def find_heroine
     @heroine = Heroine.find(params[:id])
+  end
+
+  def all_powers
+    @powers = Power.all
   end
 
   def heroine_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :powers, only: [:index, :show]
-  resources :heroines, only: [:index, :show, :new, :create]  
+  resources :heroines
+
+  ## Only "resources :heroines, only: [:index, :show, :new, :create]" is required for this coding challenge.
+  ## For the sake of compeletion and active practice, all resources for the heroines model were added.
 end
