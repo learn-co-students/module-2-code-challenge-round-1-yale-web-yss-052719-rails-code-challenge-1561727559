0e93ea9030eb318b3df9b4fc879d912f3e80b166
github username <github email address>
2019-06-28 09:50:15 -0400

    950am commit

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,31 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+  
+    if @heroine.valid?
+      redirect_to heroine_path(@heroine)
+    else
+      flash[:error] = @heroine.errros.full_messages
+      render :new
+    end
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
---
+++ db/migrate/20190628132126_add_powers_to_heroines.rb
@@ -0,0 +1,5 @@
+class AddPowersToHeroines < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :heroines, :power, foreign_key: true
+  end
+end
