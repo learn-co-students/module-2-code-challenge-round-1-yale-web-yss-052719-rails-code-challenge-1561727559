772424926fd47fb81a03906195c9dea2485ce9e1
XRuiz9 <xdruiz92@gmail.com>
2019-06-28 09:54:08 -0400

    Finished min requirements

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,32 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def new
+  end
+
+  def create
+  	@heroine = Heroine.create(heroine_params)
+
+  	if @heroine.valid?
+  		redirect_to heroine_path(@heroine)
+  	else
+  		flash.now[:error] = @heroine.errors.full_messages
+  		render :new
+  	end
+  end
+
+  def show
+  	set_heroine
+  end
+
+  private
+
+  def heroine_params
+  	params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
+  def set_heroine
+  	@heroine = Heroine.find(params[:id])
+  end
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,7 @@
 class Heroine < ApplicationRecord
+	belongs_to :power
+
+	validates :name, :super_name, :power_id, presence: true
+	validates :super_name, uniqueness: true
+
 end
