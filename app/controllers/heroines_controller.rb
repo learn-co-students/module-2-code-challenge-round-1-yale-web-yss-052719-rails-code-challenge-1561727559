6f33577519f5ec5539b50f1a76ff980a7f91b052
Elijah <efloren@siue.edu>
2019-06-28 09:49:38 -0400

    done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,35 @@
 class HeroinesController < ApplicationController
   def index
-    @heroines = Heroine.all
+    if params["heroines"] != nil
+      @power = Power.find_by(id: params["heroines"]["ids"])
+      @heroines = Heroine.all.where(power_id: @power.id)
+    else
+      @heroines = Heroine.all
+    end
+  end
+
+  def show 
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new 
+    @powers = Power.all
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.valid?
+      @heroine.save
+      redirect_to(heroine_path(@heroine))
+    else
+      render :new
+    end
+  end
+
+  private 
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
