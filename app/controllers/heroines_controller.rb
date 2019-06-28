444e04f42bc74323ef41b06840602bd515fb152c
Peter <cirediatpl@gmail.com>
2019-06-28 09:46:05 -0400

    completed up to bonus and stretch features

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,35 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    find_heroine
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.valid?
+      redirect_to heroine_path(@heroine)
+    else
+      flash.now[:error] = @heroine.errors.full_messages
+      @powers = Power.all
+      render :new
+    end
+  end
+
+  private
+
+  def find_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,14 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    find_power
+  end
+
+  private
+
+  def find_power
+    @power = Power.find(params[:id])
+  end
 end
